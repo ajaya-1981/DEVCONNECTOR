e5a30eb00c0d186871cc03c813b213157c4375cb
ajayanayak-axtria <ajaya.nayak@axtria.com>
2021-07-08 18:53:30 +0530

    testing

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('API Running');
+  res.send('API Running for Testing');
 });
 
 const PORT = process.env.PORT || 5000;
