48deaf238389a8f879adb15d9bbd8dbdcc2312af
ajayanayak-axtria <ajaya.nayak@axtria.com>
2021-07-15 17:31:18 +0530

    adding react code for devconnector

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, { Fragment } from 'react';
+import Navbar from './components/layouts/Navbar';
+import Landing from './components/layouts/Landing';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alert from './components/layouts/Alert';
+//redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+import './App.css';
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Alert></Alert>
+          <Switch>
+            <Route exact path="/register" component={Register}></Route>
+            <Route exact path="/login" component={Login}></Route>
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
+);
+
+export default App;
